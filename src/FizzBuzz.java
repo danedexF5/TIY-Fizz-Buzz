6546e75a38a979b0134a86dff939b6f582753577
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-04-13 21:40:41 -0400

    update fizzbuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+/**
+ * Created by danedexheimer on 4/13/16.
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int x = 1; x <= 100; x++){
+
+            if (x % 3 == 0)
+                System.out.println("Fizz");
+
+                else if
+                    (x % 5 == 0)
+                System.out.println("Buzz");
+
+                else if
+                    (x % 15 == 0)
+                System.out.println("FizzBuzz");
+
+        }
+    }
+}
