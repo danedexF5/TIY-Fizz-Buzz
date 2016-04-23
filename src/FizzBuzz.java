8cb7e6156b49ceac6fb08d78b863f59b811b72c3
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-04-23 17:53:38 -0400

    revisions to fizzbuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,18 +4,20 @@
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int x = 1; x <= 100; x++){
+        for (int x = 1; x <= 100; x++) {
 
-            if (x % 3 == 0)
-                System.out.println("Fizz");
+            if
+                    ((x % 3 == 0) && (x % 5 == 0))
+                System.out.println(x + "FizzBuzz");
 
-                else if
+            else if
+                    (x % 3 == 0)
+                System.out.println(x + "Fizz");
+
+            else if
                     (x % 5 == 0)
-                System.out.println("Buzz");
-
-                else if
-                    (x % 15 == 0)
-                System.out.println("FizzBuzz");
+                System.out.println(x + "Buzz");
+            else System.out.println(x);
 
         }
     }
